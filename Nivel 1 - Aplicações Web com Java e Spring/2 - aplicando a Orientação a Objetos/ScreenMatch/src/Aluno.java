dbe7f3db3ba8a7f5b553968e70709d1f985f5db9
Lucas Nery Miranda <lucasnerym@gmail.com>
2026-07-15 19:44:27 -0300

    Delete Nivel 1 - Aplicações Web com Java e Spring/2 - aplicando a Orientação a Objetos/ScreenMatch/src/Aluno.java

--- Nivel 1 - Aplicações Web com Java e Spring/2 - aplicando a Orientação a Objetos/ScreenMatch/src/Aluno.java
+++
@@ -1,9 +0,0 @@
-public class Aluno {
-    String nome;
-    int idade;
-
-    void exibirInformacoes(){
-        System.out.println("O nome do aluno é: " + nome);
-        System.out.println("A idade do aluno é: " + idade);
-    }
-}
