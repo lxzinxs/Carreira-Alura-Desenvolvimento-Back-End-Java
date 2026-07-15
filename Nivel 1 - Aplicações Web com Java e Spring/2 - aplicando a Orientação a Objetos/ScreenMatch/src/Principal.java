a56097f1586821f2d5b4ce2d6afe04a3b0b92cfb
Lucas Nery Miranda <lucasnerym@gmail.com>
2026-07-15 19:41:13 -0300

    Add files via upload

--- Nivel 1 - Aplicações Web com Java e Spring/2 - aplicando a Orientação a Objetos/ScreenMatch/src/Principal.java
+++ Nivel 1 - Aplicações Web com Java e Spring/2 - aplicando a Orientação a Objetos/ScreenMatch/src/Principal.java
@@ -1,16 +1,28 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoLancamento = 1970;
-        meuFilme.duracaoMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoLancamento(1970);
+        meuFilme.setDuracaoMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoMinutos());
 
         meuFilme.exibiFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
         System.out.println("Total de avaliaçoes: " + meuFilme.getTotalAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie serie = new Serie();
+        serie.setNome("Lost");
+        serie.setAnoLancamento(2000);
+        serie.exibiFichaTecnica();
+        serie.setTemporadas(10);
+        serie.setEpisodiosPorTemporada(10);
+        serie.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar serie: " + serie.getDuracaoMinutos());
     }
 }
--- Nivel 1 - Aplicações Web com Java e Spring/2 - aplicando a Orientação a Objetos/ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ Nivel 1 - Aplicações Web com Java e Spring/2 - aplicando a Orientação a Objetos/ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,28 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    public String nome;
-    public int anoLancamento;
-    public boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalAvaliacoes;
-    public int duracaoMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public int getTotalAvaliacoes(){
-        return totalAvaliacoes;
+    public void setDiretor(String diretor){
+        this.diretor = diretor;
     }
 
-    public void exibiFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoLancamento);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalAvaliacoes++;
-    }
-
-    public double pegaMedia() {
-        return somaDasAvaliacoes / totalAvaliacoes;
+    public String getDiretor(){
+        return diretor;
     }
 }
