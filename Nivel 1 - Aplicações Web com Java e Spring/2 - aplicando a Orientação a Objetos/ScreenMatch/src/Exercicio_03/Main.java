aa2c76817b3f0ebe825c8e25c9fe20c24ec58a79
Lucas Nery Miranda <lucasnerym@gmail.com>
2026-07-23 18:32:23 -0300

    Add files via upload

--- Nivel 1 - Aplicações Web com Java e Spring/2 - aplicando a Orientação a Objetos/ScreenMatch/src/Exercicio_03/Main.java
+++ Nivel 1 - Aplicações Web com Java e Spring/2 - aplicando a Orientação a Objetos/ScreenMatch/src/Exercicio_03/Main.java
@@ -31,8 +31,17 @@ public class Main {
         contaCorrente.consultarSaldo();
         contaCorrente.sacar(1000);
         contaCorrente.consultarSaldo();
+        System.out.println("\n");
 
         //Exercicio_04
-        
+        VerificadorPrimo verificador = new VerificadorPrimo();
+        verificador.verificarSeEhPrimo(17);
+
+        GeradorPrimo gerador = new GeradorPrimo();
+        int proximoPrimo = gerador.gerarProximoPrimo(17);
+        System.out.println("O próximo primo após 17 é: " + proximoPrimo);
+
+        NumerosPrimos numerosPrimos = new NumerosPrimos();
+        numerosPrimos.listarPrimos(30);
     }
 }
